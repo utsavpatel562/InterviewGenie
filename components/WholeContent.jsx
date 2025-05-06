e7c46f6cb217a88b104bd93377b0831ea8428a4f
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-05-06 09:34:36 -0400

    first commit

--- components/WholeContent.jsx
+++ components/WholeContent.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-
+import { RiSpeakAiLine } from "react-icons/ri";
+import { RiBarChartBoxAiLine } from "react-icons/ri";
+import { TbReportAnalytics } from "react-icons/tb";
+import { RiSave2Line } from "react-icons/ri";
 function WholeContent() {
   return (
     <>
@@ -14,26 +17,66 @@ function WholeContent() {
               />
             </div>
             <div className="max-w-xl px-4 space-y-3 mt-6 sm:px-0 md:mt-0 lg:max-w-2xl">
-              <h2 className="text-4xl font-bold text-gray-700 mb-6">
+              {/*<h2 className="text-4xl font-bold text-gray-700 mb-6">
                 How It Works
               </h2>
-
+            */}
               <p className="text-gray-800 text-3xl font-semibold sm:text-4xl">
                 Ace Your Interview in 3 Simple Steps
               </p>
               <h3 className="text-blue-500 font-semibold">
                 Practice. Speak. Improve. Repeat.
               </h3>
-              <div className="mt-6">
-                <div className="bg-gray-100 border-1 border-gray-100 rounded-lg p-4">
-                  <h2 className="text-lg font-bold text-slate-900">
+              <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-3">
+                <div className="border-2 border-gray-200 rounded-lg p-4">
+                  <RiSpeakAiLine className="bg-blue-50 w-8 h-8 p-1 text-blue-400 rounded-sm border" />
+                  <h2 className="text-lg font-bold text-slate-900 mt-2">
                     Speak your answers live
                   </h2>
+                  <p className="text-gray-600">
+                    Practice interviews just like the real thing - using your
+                    voice, not buttons. Simulate actual conversation flow and
+                    build confidence.
+                  </p>
+                </div>
+
+                <div className="border-2 border-gray-200 rounded-lg p-4">
+                  <RiBarChartBoxAiLine className="bg-blue-50 w-8 h-8 p-1 text-blue-400 rounded-sm border" />
+                  <h2 className="text-lg font-bold text-slate-900 mt-2">
+                    Speak your answers live
+                  </h2>
+                  <p className="text-gray-600">
+                    Practice interviews just like the real thing - using your
+                    voice, not buttons. Simulate actual conversation flow and
+                    build confidence.
+                  </p>
+                </div>
+                <div className="border-2 border-gray-200 rounded-lg p-4">
+                  <TbReportAnalytics className="bg-blue-50 w-8 h-8 p-1 text-blue-400 rounded-sm border" />
+                  <h2 className="text-lg font-bold text-slate-900 mt-2">
+                    Speak your answers live
+                  </h2>
+                  <p className="text-gray-600">
+                    Practice interviews just like the real thing - using your
+                    voice, not buttons. Simulate actual conversation flow and
+                    build confidence.
+                  </p>
+                </div>
+                <div className="border-2 border-gray-200 rounded-lg p-4">
+                  <RiSave2Line className="bg-blue-50 w-8 h-8 p-1 text-blue-400 rounded-sm border" />
+                  <h2 className="text-lg font-bold text-slate-900 mt-2">
+                    Speak your answers live
+                  </h2>
+                  <p className="text-gray-600">
+                    Practice interviews just like the real thing - using your
+                    voice, not buttons. Simulate actual conversation flow and
+                    build confidence.
+                  </p>
                 </div>
               </div>
               <a
                 href="#"
-                className="inline-flex gap-x-1 items-center text-blue-500 hover:text-blue-600 duration-150 font-medium"
+                className="inline-flex mt-2 mb-2 hover:bg-blue-50 p-2 rounded-full gap-x-1 items-center text-blue-500 hover:text-blue-600 duration-150 font-medium"
               >
                 Learn more
                 <svg
