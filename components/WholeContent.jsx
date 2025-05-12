0c75d4b84705916bffedd7ee683e1459ee6c20e6
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-05-12 08:20:08 -0400

    first commit

--- components/WholeContent.jsx
+++ components/WholeContent.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { RiSpeakAiLine } from "react-icons/ri";
+import { RiLinkedinBoxFill, RiSpeakAiLine } from "react-icons/ri";
 import { RiBarChartBoxAiLine } from "react-icons/ri";
 import { TbReportAnalytics } from "react-icons/tb";
 import { RiSave2Line } from "react-icons/ri";
 import { TbAugmentedReality } from "react-icons/tb";
 import { RiVoiceAiLine } from "react-icons/ri";
-import { SiCodementor } from "react-icons/si";
+import { SiCodementor, SiGithub } from "react-icons/si";
 import { GiProgression } from "react-icons/gi";
 function WholeContent() {
   const features = [
@@ -24,6 +24,26 @@ function WholeContent() {
     {
       icon: <GiProgression />,
       title: "Progress Tracking Dashboard",
+    },
+  ];
+  const contactMethods = [
+    {
+      icon: <RiLinkedinBoxFill />,
+      title: "Connect on LinkedIn",
+      desc: "Have questions or want to collaborate? Reach out to me!",
+      link: {
+        name: "Connect",
+        href: "https://www.linkedin.com/in/utsav-patel-coder",
+      },
+    },
+    {
+      icon: <SiGithub />,
+      title: "Follow my GitHub",
+      desc: "Check out our latest projects and contributions on GitHub",
+      link: {
+        name: "Visit GitHub",
+        href: "https://github.com/utsavpatel562",
+      },
     },
   ];
   return (
@@ -155,6 +175,56 @@ function WholeContent() {
             background: "linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%)",
           }}
         ></div>
+      </section>
+      <section className="py-5">
+        <div className="max-w-screen-xl mx-auto px-4 text-gray-600 gap-12 md:px-8 lg:flex bg-slate-50 p-12 rounded-2xl border-2 shadow-2xl">
+          <div className="max-w-md">
+            <h3 className="text-gray-800 text-3xl font-semibold sm:text-4xl">
+              Let’s connect
+            </h3>
+            <p className="mt-3">
+              I'm here to help and answer any question you might have, looking
+              forward to hearing from you .
+            </p>
+          </div>
+          <div>
+            <ul className="mt-12 gap-y-6 gap-x-12 items-center md:flex lg:gap-x-0 lg:mt-0">
+              {contactMethods.map((item, idx) => (
+                <li
+                  key={idx}
+                  className="space-y-3 border-t py-6 md:max-w-sm md:py-0 md:border-t-0 lg:border-l lg:px-12 lg:max-w-none"
+                >
+                  <div className="w-12 h-12 rounded-full border flex items-center justify-center text-gray-700">
+                    {item.icon}
+                  </div>
+                  <h4 className="text-gray-800 text-lg font-medium xl:text-xl">
+                    {item.title}
+                  </h4>
+                  <p>{item.desc}</p>
+                  <a
+                    target="_blank"
+                    href={item.link.href}
+                    className="flex items-center gap-1 text-sm text-indigo-600 duration-150 hover:text-indigo-400 font-medium"
+                  >
+                    {item.link.name}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      className="w-5 h-5"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </section>
     </>
   );
