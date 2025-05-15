3699f8674a871d803fcc534ea4bfc068430f65fd
200584363@student.georgianc.on.ca <utsavpatel562@gmail.com>
2025-05-15 13:19:31 -0400

    first commit

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -1,11 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
-
+import { FcGoogle } from "react-icons/fc";
 function login() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center">
-        <div>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center rounded-2xl border p-8">
           <Image
             src="/logo.png"
             alt="logo"
@@ -13,6 +14,29 @@ function login() {
             height={100}
             className="w-[220px]"
           />
+          <div className="flex flex-col items-center">
+            <Image
+              src={"/img4.png"}
+              alt="login"
+              width={600}
+              height={400}
+              className="w-[400px] h-[250px] object-contain rounded-2xl my-4 border"
+              style={{
+                background:
+                  "linear-gradient(to bottom, rgba(255,255,255,0.15) 0%, rgba(0,0,0,0.15) 100%), radial-gradient(at top center, rgba(255,255,255,0.40) 0%, rgba(0,0,0,0.40) 120%) #989898",
+              }}
+            />
+            <h2 className="text-2xl font-bold text-center text-slate-900">
+              Welcome to InterviewGenie
+            </h2>
+            <p className="text-gray-500 font-semibold text-center">
+              Sign In with Google Authentication
+            </p>
+            <Button className="flex w-full hover:bg-slate-900 items-center my-3 cursor-pointer bg-slate-800">
+              Login with Google
+              <FcGoogle />
+            </Button>
+          </div>
         </div>
       </div>
     </>
